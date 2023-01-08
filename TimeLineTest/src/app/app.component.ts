93f97fdb4d2980de548b6c70fd2a8683356d997d
olegx100 <olegx100@gmail.com>
2023-01-08 17:15:27 +0200

    Minor changes - createSeries func in app.component

--- TimeLineTest/src/app/app.component.ts
+++ TimeLineTest/src/app/app.component.ts
@@ -9,48 +9,9 @@ export class AppComponent {
   title = 'TimeLineTest';
 
   public Series: Array<any>;
- 
+  
   constructor () {
-    this.Series = [];
-
-    this.Series.push ({ 
-      "type":'time-point', 
-      "items": this.createDummyModel(1000), 
-      "legend": {
-        "borderColor": "coral", 
-        "fillColor": "aqua", 
-        "pointSize": 11
-      }
-    });
-
-    this.Series.push ({ "type":'time-scale'});
-
-    this.Series.push ({ 
-      "type":'time-point', 
-      "items": this.createDummyModel(1000), 
-      "legend": {
-        "borderColor": "red", 
-        "fillColor": "pink", 
-        "pointSize": 9
-      }
-    });
-
-    this.Series.push ({ "type":'time-span', "items":  this.createDummyModel(1000) });
-
-    this.Series.push ({ "type":'time-scale'});
-
-    this.Series.push ({ 
-      "type":'time-point', 
-      "items": this.createDummyModel(1000), 
-      "legend": {
-        "borderColor": "blue", 
-        "fillColor": "aqua", 
-        "pointSize": 13
-      }
-    });
-
-    this.Series.push ({ "type":'time-scale'});
-   
+    this.createSeries();
   }
 
   getItems () {
@@ -110,5 +71,47 @@ export class AppComponent {
     }
 
     return states;
-}
+  }
+
+  createSeries (): void {
+    this.Series = [];
+
+    this.Series.push ({ 
+      "type":'time-point', 
+      "items": this.createDummyModel(1000), 
+      "legend": {
+        "borderColor": "coral", 
+        "fillColor": "aqua", 
+        "pointSize": 11
+      }
+    });
+
+    this.Series.push ({ "type":'time-scale'});
+
+    this.Series.push ({ 
+      "type":'time-point', 
+      "items": this.createDummyModel(1000), 
+      "legend": {
+        "borderColor": "red", 
+        "fillColor": "pink", 
+        "pointSize": 9
+      }
+    });
+
+    this.Series.push ({ "type":'time-span', "items":  this.createDummyModel(1000) });
+
+    this.Series.push ({ "type":'time-scale'});
+
+    this.Series.push ({ 
+      "type":'time-point', 
+      "items": this.createDummyModel(1000), 
+      "legend": {
+        "borderColor": "blue", 
+        "fillColor": "aqua", 
+        "pointSize": 13
+      }
+    });
+
+    this.Series.push ({ "type":'time-scale'});
+  }
 }
