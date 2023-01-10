f02ef1d46bdfb9aa5b896bfabb8b0c634e3ca44d
olegx100 <olegx100@gmail.com>
2023-01-10 22:16:51 +0200

    implement touch support

--- TimeLineTest/src/time-line/time-line-container/time-line-container.component.ts
+++ TimeLineTest/src/time-line/time-line-container/time-line-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { toArray } from 'rxjs';
 import { GraphDateScale } from '../GraphDateScale';
 
 @Component({
@@ -24,6 +25,13 @@ export class TimeLineContainerComponent {
     this.timeLineContainer.addEventListener("mousedown", this.startDrag.bind(this));
     this.timeLineContainer.addEventListener("mouseup", this.endDrag.bind(this));
     this.timeLineContainer.addEventListener('mousemove', this.drag.bind(this));
+
+    this.timeLineContainer.addEventListener("touchstart", this.touchDown.bind(this));
+    this.timeLineContainer.addEventListener('touchmove', this.touchMove.bind(this));
+    //touchup is not called after touchmove
+    //this.timeLineContainer.addEventListener("touchup", this.endDrag.bind(this));
+
+
 
     this.timeScale.widthPx = this.timeLineContainer.clientWidth;
     window.addEventListener ('resize', this.onWndResize.bind(this));
@@ -52,6 +60,73 @@ export class TimeLineContainerComponent {
   }
 
   endDrag (evt: any) {
+    console.log ("touchUp");
     this.timeScale.endDrag();
+  }
+
+  touches: Array<any>;
+  touchMove (evt: any) {
+    this.procTouchMove (evt);
+  }
+  
+  touchDown (evt: any) {
+    const offsetInPx = evt.touches[0].pageX - this.timeLineContainer.getBoundingClientRect().left;
+    this.timeScale.startDrag(offsetInPx);
+    this.touches = evt.touches;
+  }
+
+  getTouchChanges (tarr: Array<any>) {
+    let res = new Array<any>();
+    let i = 0;
+    while (i < this.touches.length) {
+      let j = 0;
+      while (j < tarr.length) {
+        if (this.touches[i].identifier == tarr[j].identifier) {
+          let pt = {
+            "id": this.touches[i].identifier,
+            "fromX": this.touches[i].pageX,
+            "toX": tarr[i].pageX
+          }
+          res.push(pt);
+        }
+        j++
+      }
+      i++;
+    }
+    return res;
+  }
+
+  procTouchMove (evt: any): void {
+    var tArr = this.getTouchChanges (evt.touches);
+    if (tArr.length < 1)
+      return;
+
+    const offsetInPx = evt.touches[0].pageX - this.timeLineContainer.getBoundingClientRect().left;  
+    if (tArr.length == 1)  {
+      this.timeScale.drag(offsetInPx, 1);
+      return;
+    }
+
+    let min = tArr[0];
+    let max = tArr[1];
+    for (let i = 0; i < tArr.length; i++) {
+      if (min.toX - min.fromX > tArr[i].toX - tArr[i].fromX)
+        min = tArr[i];
+      
+      if (max.toX - max.fromX > tArr[i].toX - tArr[i].fromX)
+        max = tArr[i];        
+    }
+    
+    let scale = Math.abs (min.fromX - max.fromX);
+    if (!scale)
+      scale = 1;
+    
+    scale = Math.abs (min.toX - max.toX)  / scale;
+    if (!scale)
+      scale = 1;
+
+    let midPoint = (min.toX + max.toX) / 2 - offsetInPx;
+    this.timeScale.changeScale(midPoint, scale);
+    this.touches = evt.touches;
   }
 }
